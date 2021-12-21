6bb14731a92d7e3d660297af321383ed32db375c
bimri <bimri@outlook.com>
2021-12-21 15:14:48 +0300

    newly formated - yapf - file

--- 01_hello/hello.py
+++ 01_hello/hello.py
@@ -10,8 +10,11 @@ import argparse
 def get_args():
     """Get the command-line arguments"""
     parser = argparse.ArgumentParser(description='Say hello')
-    parser.add_argument('-n', '--name', metavar='name',
-                        default='World', help='Name to greet')
+    parser.add_argument('-n',
+                        '--name',
+                        metavar='name',
+                        default='World',
+                        help='Name to greet')
     return parser.parse_args()
 
 
