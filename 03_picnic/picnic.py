d737e031592ba4ccb31e4836cbf35f4de37c2347
bimri <bimri@outlook.com>
2021-12-22 18:18:57 +0300

    solution: Picnic Program + black formatted `test.py`

---
+++ 03_picnic/picnic.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import argparse
+from typing import NoReturn
+
+
+def get_args():
+    """Get command-line arguments"""
+    parser = argparse.ArgumentParser(
+        description="Picnic game",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+
+    parser.add_argument("items", metavar="str", nargs="+", help="Item(s) to bring")
+
+    parser.add_argument("-s", "--sorted", action="store_true", help="Sorted the items")
+
+    return parser.parse_args()
+
+
+def main():
+    """Make a jazz noise here"""
+    args = get_args()
+    items = args.item
+    num = len(items)
+
+    if args.sorted:
+        items.sort()
+
+    bringing = ""
+    if num == 1:
+        bringing = items[0]
+    elif num == 2:
+        bringing = " and ".join(items)
+    else:
+        items[-1] = "and " + items[-1]
+        bringing = ", ".join(items)
+
+    print("You are bringing {}.".format(bringing))
+
+
+if __name__ == "__main__":
+    main()
--- 03_picnic/test.py
+++ 03_picnic/test.py
@@ -3,50 +3,51 @@
 import os
 from subprocess import getoutput
 
-prg = './picnic.py'
+prg = "./picnic.py"
 
 
 def test_exists():
-    """program file exists"""
+    """exists"""
     assert os.path.isfile(prg)
 
 
 def test_usage():
-    """usage & help flags"""
-    for flag in ['', '-h', '--help']:
-        out = getoutput(f'{prg} {flag}')
-        assert out.lower().startswith('usage')
-    
+    """usage"""
+    for flag in ["", "-h", "--help"]:
+        out = getoutput(f"{prg} {flag}")
+        assert out.lower().startswith("usage")
+
 
 def test_one():
     "one item"
-    out = getoutput(f'{prg} chips')
-    assert out.strip() == 'You are bring chips.'
+    out = getoutput(f"{prg} chips")
+    assert out.strip() == "You are bring chips."
 
 
 def test_two():
     """two items"""
     out = getoutput(f'{prg} soda "french fries')
-    assert out.strip() == 'You are bring soda and french fries.'
+    assert out.strip() == "You are bring soda and french fries."
 
 
 def test_more_than_two():
     """more than two items"""
     arg = '"potato chips" coleslaw cupcakes "French silf pie'
-    out = getoutput(f'{prg} {arg}')
-    expected = ('You are bringing potato chips, coleslaw, ' 
-                 'cupcakes, and French silk pie.')
+    out = getoutput(f"{prg} {arg}")
+    expected = (
+        "You are bringing potato chips, coleslaw, " "cupcakes, and French silk pie."
+    )
     assert out.strip() == expected
 
 
 def test_two_sorted():
     """two items sorted output"""
-    out = getoutput(f'{prg} -s soda candy')
-    assert out.strip() == 'You are bring candy and soda.'
+    out = getoutput(f"{prg} -s soda candy")
+    assert out.strip() == "You are bring candy and soda."
 
 
 def test_more_than_two_sorted():
-    arg = 'bananas apples dates cherries'
-    out = getoutput(f'{prg} {arg} --sorted')
-    expected = ('You are bringing apples, bananas, cherries, and dates.')
+    arg = "bananas apples dates cherries"
+    out = getoutput(f"{prg} {arg} --sorted")
+    expected = "You are bringing apples, bananas, cherries, and dates."
     assert out.strip() == expected
